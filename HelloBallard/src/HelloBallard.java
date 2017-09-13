2a8b970bb361bf2174eff0036d13e16486f0738f
Straitjacket Roland <rpschoeneman@gmail.com>
2017-09-13 13:27:36 -0700

    Created a Hello Ballard Project
    It says hello to the geographical location known as ballard.

---
+++ HelloBallard/src/HelloBallard.java
@@ -0,0 +1,16 @@
+/**
+ * Created by bal_rpschoeneman on 9/12/2017.
+ */
+public class HelloBallard {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello Ballard!!!!");
+
+    }
+
+
+}
+
+
+
