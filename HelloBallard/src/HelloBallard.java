9adc0b72806eb51be987b2130f8839e85aeb3613
Straitjacket Roland <31781296+bigr0@users.noreply.github.com>
2017-09-22 15:00:11 -0700

    Created Variable Practice
    Used instruction and my own intelligence to print variables, cool.

--- HelloBallard/src/HelloBallard.java
+++ HelloBallard/src/HelloBallard.java
@@ -5,7 +5,7 @@ public class HelloBallard {
 
     public static void main(String[] args) {
 
-        System.out.println("Hello Ballard!!!!");
+        System.out.println("Hello Ballard!");
 
     }
 
---
+++ HelloBallard/src/VariablePractice.java
@@ -0,0 +1,11 @@
+public class VariablePractice {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 3;
+        a=b;
+        b = b + 2;
+
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
