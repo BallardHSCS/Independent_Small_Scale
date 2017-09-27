a5e0bde8aef5666c480941fd27404ac4ac2a8c98
Straitjacket Roland <31781296+bigr0@users.noreply.github.com>
2017-09-27 13:20:51 -0700

    Made autobart
    I didnt really, but i know how

---
+++ AutoBart/src/AutoBart.java
@@ -0,0 +1,15 @@
+public class AutoBart {
+
+public static void main(String[] args){
+
+
+
+
+  public static void writeOneLine(){
+
+  }
+
+
+
+
+}
---
+++ HelloBallard/src/AutoBart.java
@@ -0,0 +1,21 @@
+public class AutoBart {
+
+        public static void main(String[] args){
+
+            System.out.println("I will not hide the teacher's Viagra");
+            System.out.println("I will not hide the teacher's Viagra");
+            System.out.println("I will not hide the teacher's Viagra");
+            System.out.println("");
+            System.out.println("");
+            System.out.println("");
+            System.out.println("");
+            System.out.println("");
+            System.out.println("");
+            System.out.println("");
+
+
+
+        }
+
+
+}
