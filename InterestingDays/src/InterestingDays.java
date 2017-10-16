8bb35a3eab82f0ee4866cceb8c69bc75bcbc7ddd
Straitjacket Roland <31781296+bigr0@users.noreply.github.com>
2017-10-16 14:37:06 -0700

    Comments
    comments

--- InterestingDays/src/InterestingDays.java
+++ InterestingDays/src/InterestingDays.java
@@ -3,7 +3,7 @@ import java.util.GregorianCalendar;
 
 public class InterestingDays {
     public static void main(String args[]){
-
+//gets the important dates i need and uses daysFrom to calculate distance
        Day today = new Day();
        Day myBirthday = new Day(2001, 10, 29);
        Day myBirthdayThisYear = new Day(2017,10, 29 );
@@ -12,12 +12,12 @@ public class InterestingDays {
        int apTestIn = apDay.daysFrom(today);
        int DaysAlive = myBirthday.daysFrom(today);
 
-
+         //tells user what the numbers mean and prints out the values
        System.out.println("It is my Bday in " + myBirthdayIn +" days!" );
        System.out.println("AP test in "+ apTestIn + " days!");
        System.out.println("The current date is "+ today);
        System.out.println("I have been alive for " + DaysAlive*-1 + " Days");
-
+         //tells user my birthday so they dont forget as they always do ;-;
 
 
 
