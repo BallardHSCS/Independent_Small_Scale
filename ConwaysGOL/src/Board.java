8eed7b9ebda9a96e16253362315f019ba3d72c6f
Straitjacket Roland <31781296+bigr0@users.noreply.github.com>
2018-02-16 15:03:18 -0800

    AAAAAAAA
    ConwaysGOL is hard, array logic hurts

---
+++ ConwaysGOL/src/Board.java
@@ -0,0 +1,64 @@
+public class Board {
+
+    public static final int SIZE = 20;
+    String[][] boardArray = new String[SIZE][SIZE];
+    BoardReader board;
+
+    public Board(String fileName) {
+        BoardReader boardPrint = new BoardReader(fileName);
+        board = boardPrint;
+        for (int row = 0; row < boardArray.length; row++) {
+            for (int col = 0; col < boardArray[row].length; col++) {
+                boardArray[row][col] = boardPrint.getNextCell();
+            }
+        }
+    }
+
+    public String toString() {
+        String output = "";
+        for (int row = 0; row < boardArray.length; row++) {
+            for (int col = 0; col < boardArray[row].length; col++) {
+                output += boardArray[row][col];
+
+
+            }
+            output += "\n";
+        }
+        return output;
+    }
+
+    private int getNeighbors(int rowPos, int colPos) {
+        int neighborCount = 0;
+        int startRow = rowPos - 1;
+        int endRow = rowPos + 1;
+        int startCol = colPos - 1;
+        int endCol = colPos + 1;
+
+        if(rowPos == 0){
+            startRow=0;
+        }
+        if(rowPos == boardArray.length){
+            endRow = boardArray.length;
+        }
+        if (colPos == 0){
+            startCol = 0;
+        }
+        if(colPos == boardArray.length){
+            endCol = boardArray.length;
+        }
+
+        for (int row = startRow; row == endRow; row++) {
+            for(int col = startCol; col == endCol; col++){
+                 if(){}
+
+                 if(){}
+
+            }
+        }
+
+
+        return neighborCount;
+    }
+
+
+}
